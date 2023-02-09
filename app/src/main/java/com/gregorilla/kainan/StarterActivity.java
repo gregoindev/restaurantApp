61e02c449e294f25662f3c68e0a64f3f3999bacd
gregoindev <gregocodes23@gmail.com>
2023-02-09 04:19:22 -0800

    Initial commit

---
+++ app/src/main/java/com/gregorilla/kainan/StarterActivity.java
@@ -0,0 +1,19 @@
+package com.gregorilla.kainan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class StarterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starter);
+
+        ListView startersList = findViewById(R.id.list_view_starters);
+
+        String[] dishes = new String[20];
+    }
+}
