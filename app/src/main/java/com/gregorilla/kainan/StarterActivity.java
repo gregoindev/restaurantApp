666400fdaa1dad25b71dc53dbd454d10e3948949
gregoindev <gregocodes23@gmail.com>
2023-02-10 04:47:45 -0800

    Main Course activity

--- app/src/main/java/com/gregorilla/kainan/StarterActivity.java
+++ app/src/main/java/com/gregorilla/kainan/StarterActivity.java
@@ -3,6 +3,7 @@ package com.gregorilla.kainan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class StarterActivity extends AppCompatActivity {
@@ -14,6 +15,20 @@ public class StarterActivity extends AppCompatActivity {
 
         ListView startersList = findViewById(R.id.list_view_starters);
 
-        String[] dishes = new String[20];
+        Dish[] dishes = {
+                new Dish("Mushroom and tofu maki", "Toasted sushi, rice filled with chest mushroom and tofu", 999),
+                new Dish("Mushroom and tofu maki", "Toasted sushi, rice filled with chest mushroom and tofu", 999),
+                new Dish("Spinach and cabbage wontons", "Thin wonton cases stuffed with fresh spinach and chinese cabbage", 799),
+                new Dish("Broccoli and cucumber soup", "Fresh broccoli and cucumber combined into creamy soup", 899),
+                new Dish("Chilli and aubergine dip", "A dip made from scotch bonnet chilli and fresh aubergine", 999),
+                new Dish("Chickpea and chilli gyoza", "Thin pastry cases stuffed with fresh chickpea and green chilli", 699),
+                new Dish("Sprout and pineapple soup", "Fresh sprout and pineapple combined into creamy soup", 899),
+                new Dish("Egusi and borscht soup", "Egusi and borscht combined into creamy soup", 1299)
+
+        };
+
+
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishes);
+        startersList.setAdapter(dishesAdapter);
     }
 }
