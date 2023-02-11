53c6fb4ebc106713bcc771ccea8f9c9eb5665c70
gregoindev <gregocodes23@gmail.com>
2023-02-11 07:01:42 -0800

    desserts activity practice done

--- app/src/main/java/com/gregorilla/kainan/DessertsActivity.java
+++ app/src/main/java/com/gregorilla/kainan/DessertsActivity.java
@@ -1,6 +1,7 @@
 package com.gregorilla.kainan;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
@@ -14,6 +15,15 @@ public class DessertsActivity extends AppCompatActivity {
 
         ListView dessertsList = findViewById(R.id.list_view_desserts);
 
+        Dish[] desserts = {
+                new Dish("Cocoa and blackcurrant cake", "Moist cake made with cocoa and fresh blackcurrant", 499),
+                new Dish("Mulberry and cardamom crumble", "Fresh mulberry and green cardamom topped with butter crumble", 699),
+                new Dish("Lemon scones with chilli jam", "Vegan scones served with chilli jam and fresh coconut cream", 599),
+                new Dish("Potato and banana vegan crepes", "Crispy vegan crepes filled with fresh potato and banana", 599),
+                new Dish("Cinnamon and treacle buns", "Crumbly buns made with cinnamon and dark treacle", 499),
+        };
 
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, desserts);
+        dessertsList.setAdapter(dishesAdapter);
     }
 }
